d3ced988cb033aa5f1434d9cadd42c742209c25f
Clodagh Kelly <90684673+Clodaghkelly2001@users.noreply.github.com>
2022-02-10 10:50:23 +0000

    Week 5 jUnit testing

---
+++ Week5/src/ConvertUSDToEuro.java
@@ -0,0 +1,7 @@
+public class ConvertUSDToEuro
+{
+    public double usdToEuro(double usd, double exchangeRate)
+    {
+        throw new RuntimeException("No Product Code Written");
+    }
+}//end of convertUSDToEuro
---
+++ Week5/src/ConvertUSDToEuroTest.java
@@ -0,0 +1,18 @@
+import junit.framework.TestCase;
+
+public class ConvertUSDToEuroTest extends TestCase
+{
+
+    // Test Number: 1
+    // Test Objective: To test conversion of valid USD to Euro
+    // Test input(s): USD = 50, exchangeRate = 0.87
+    // Test expected output(s): 43.69
+
+
+
+    // Test Number: 2
+    // Test Objective: To test conversion of valid USD to Euro
+    // Test input(s): USD = -10, exchangeRate = 0.87
+    // Test expected output(s): -99
+
+}// end of ConvertUSDToEuroTest
